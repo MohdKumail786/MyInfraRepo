2524fbcaea6ae6cf4da8fd0a1aadc8bf5d15e3fa
Mohd Kumail <42214957+MohdKumail786@users.noreply.github.com>
2022-01-08 00:20:35 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     access_key = "AKIAY7FZJWKMYQ3IAQAT"
     secret_key = "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"
     bucket = "mysb-tf-state-bucket"
-    key = "main"
+    key = "terraform.tfstate"
     region = "us-east-2"
     dynamodb_table = "my-dynamodb-table"
   }
