3e6783eb3d1170afbe862943535bb795aba3fc83
Mohd Kumail <42214957+MohdKumail786@users.noreply.github.com>
2022-01-07 21:52:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ provider "aws" {
    access_key = var.access_key
    secret_key = var.secret_key
    region = var.aws_region
-   terraform { backend "s3" {} }
 }
 
 #Create security group with firewall rules
